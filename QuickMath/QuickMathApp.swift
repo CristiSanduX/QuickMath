d6ad50743061778e955b5d4023f679411ebf98ad
Cristi Sandu <cristi.sandu@csx.ro>
2023-04-05 12:33:18 +0300

    Initial Commit

---
+++ QuickMath/QuickMathApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickMathApp.swift
+//  QuickMath
+//
+//  Created by Cristi Sandu on 05.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct QuickMathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
