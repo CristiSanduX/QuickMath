e3c726b768e506625bc55b33cdc9a836b0c9934a
Cristi Sandu <cristi.sandu@csx.ro>
2023-04-07 23:19:40 +0300

    Used enum for operation buttons

--- QuickMath/ContentView.swift
+++ QuickMath/ContentView.swift
@@ -7,8 +7,17 @@
 
 import SwiftUI
 
+enum CalculatorMode {
+    case notSet
+    case addition
+    case subtraction
+    case multiplication
+}
+
 struct ContentView: View {
     @State var currentValue = "0"
+    @State var currentMode: CalculatorMode = .notSet
+    @State var lastButtonWasMode: Bool = false
     var body: some View {
         ZStack {
             Color(.black)
@@ -19,19 +28,19 @@ struct ContentView: View {
                     CalculatorButton(action: didPressNumber)
                     CalculatorButton(buttonText: "2", action: didPressNumber)
                     CalculatorButton(buttonText: "3", action: didPressNumber)
-                    CalculatorButton(buttonText: "+", color: .orange, action: didPressOperation)
+                    CalculatorButton(buttonText: "+", color: .orange, action: didPressMode, mode: .addition)
                 }
                 HStack {
                     CalculatorButton(buttonText: "4", action: didPressNumber)
                     CalculatorButton(buttonText: "5", action: didPressNumber)
                     CalculatorButton(buttonText: "6", action: didPressNumber)
-                    CalculatorButton(buttonText: "-", color: .orange, action: didPressOperation)
+                    CalculatorButton(buttonText: "-", color: .orange, action: didPressMode, mode: .subtraction)
                 }
                 HStack {
                     CalculatorButton(buttonText: "7", action: didPressNumber)
                     CalculatorButton(buttonText: "8", action: didPressNumber)
                     CalculatorButton(buttonText: "9", action: didPressNumber)
-                    CalculatorButton(buttonText: "x", color: .orange, action: didPressOperation)
+                    CalculatorButton(buttonText: "x", color: .orange, action: didPressMode, mode: .multiplication)
                 }
                 HStack {
                     CalculatorButton(buttonText: "0", width: 190, action: didPressNumber)
@@ -44,6 +53,10 @@ struct ContentView: View {
     }
     
     func didPressNumber(button: CalculatorButton) {
+        if lastButtonWasMode {
+            lastButtonWasMode = false
+        }
+        
         if let parsedValue = Int(currentValue+button.buttonText) {
             currentValue = "\(parsedValue)"
         }
@@ -51,11 +64,14 @@ struct ContentView: View {
             currentValue = "Error"
         }
     }
-    func didPressOperation(button: CalculatorButton) {
-        
+    func didPressMode(button: CalculatorButton) {
+        currentMode = button.mode
+        lastButtonWasMode = true
     }
     func didPressEqual(button: CalculatorButton) {
-        
+        if currentMode == .notSet || lastButtonWasMode {
+            return
+        }
     }
     func didPressClear(button: CalculatorButton) {
         currentValue = "0"
