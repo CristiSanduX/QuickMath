2a0a7a08c7eb6b1380d630c80179a54b9bc8e273
Cristi Sandu <cristi.sandu@csx.ro>
2023-04-07 23:42:55 +0300

    Formatted the number with commas

--- QuickMath/ContentView.swift
+++ QuickMath/ContentView.swift
@@ -99,7 +99,12 @@ struct ContentView: View {
         if currentMode == .notSet {
             savedNumber = currentValueInt
         }
-        currentValue = "\(currentValueInt)"
+        
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        let num = NSNumber(value: currentValueInt)
+        
+        currentValue = numberFormatter.string(from: num) ?? "Error"
     }
 }
 
